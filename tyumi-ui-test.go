2d6e1a38ebcb38ffbcbb434500a7b8f0c8cea066
Benjamin Nicholls <nicholls.ben@gmail.com>
2025-01-08 18:01:07 -0500

    page container, choicebox, animations, and more!

--- tyumi-ui-test.go
+++ tyumi-ui-test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/bennicholls/tyumi/engine"
 	"github.com/bennicholls/tyumi/event"
+	"github.com/bennicholls/tyumi/gfx"
 	"github.com/bennicholls/tyumi/gfx/col"
 	"github.com/bennicholls/tyumi/gfx/ui"
 	"github.com/bennicholls/tyumi/input"
@@ -15,7 +16,7 @@ import (
 )
 
 func main() {
-	engine.InitConsole(40, 20)
+	engine.InitConsole(40, 30)
 	platform.Set(platform_sdl.New())
 	err := engine.SetupRenderer("res/curses24x24.bmp", "res/font12x24.bmp", "TEST WINDOW")
 	if err != nil {
@@ -25,7 +26,6 @@ func main() {
 
 	state := TestState{}
 	state.Setup()
-
 	engine.InitMainState(&state)
 
 	engine.Run()
@@ -34,45 +34,41 @@ func main() {
 type TestState struct {
 	engine.StatePrototype
 
-	text ui.Textbox
-	list ui.List
+	tabs        *ui.PageContainer
+	moving_text *ui.Textbox
+	list        *ui.List
 
 	tick int
 }
 
 func (ts *TestState) Setup() {
 	ts.Init(engine.FIT_CONSOLE, engine.FIT_CONSOLE)
+	ts.AddInputHandler(ts.HandleInputs)
 	ts.Window().SetDefaultColours(col.Pair{col.RED, col.LIME})
 	ts.Window().SetBorderStyle(ui.BORDER_STYLE_CUSTOM, ui.BorderStyles["Thick"])
 
-	container := ui.ElementPrototype{}
-	container.Init(20, 15, vec.Coord{1, 1}, 1)
-	container.EnableBorder("Container", "")
+	ts.tabs = ui.NewPageContainer(ts.Window().Bounds().W-2, ts.Window().Bounds().H-2, vec.Coord{1, 1}, 0)
+	ts.tabs.SetupBorder("UI TESTS!!", "tab to swap")
+	ts.tabs.SetDefaultColours(col.Pair{col.MAROON, col.DARKGREY})
+	ts.Window().AddChild(ts.tabs)
 
-	ts.text = ui.NewTextbox(ui.FIT_TEXT, ui.FIT_TEXT, vec.Coord{1, 1}, 0, "TEST STRING DO NOT UPVOTE", true)
-	ts.text.SetDefaultColours(col.Pair{col.CYAN, col.FUSCHIA})
-	ts.text.EnableBorder("TEST TITLE", "hint")
-	ts.text.SetBorderStyle(ui.BORDER_STYLE_INHERIT)
+	//
+	//some basic elements with input handlers
+	//
+	page1 := ts.tabs.CreatePage("input")
 
-	text2 := ui.NewTextbox(10, ui.FIT_TEXT, vec.Coord{7, 3}, 0, util.LoremIpsum(31), true)
-	text2.EnableBorder("", "")
-	container.AddChildren(&ts.text, &text2)
-	ts.AddInputHandler(ts.HandleInputs)
-
-	ts.Window().AddChild(&container)
-
-	inputbox := ui.NewInputbox(10, 1, vec.Coord{25, 18}, 10)
-	inputbox.EnableBorder("inputs!", "do the input")
+	inputbox := ui.NewInputbox(10, 1, vec.Coord{25, 18}, 2)
+	inputbox.SetupBorder("type some text", "use your hands")
 	inputbox.SetBorderStyle(ui.BORDER_STYLE_CUSTOM, ui.BorderStyles["Block"])
-	ts.Window().AddChild(&inputbox)
+	page1.AddChild(inputbox)
 
 	ts.list = ui.NewList(15, 10, vec.Coord{8, 8}, 1)
 	for i := 0; i < 20; i++ {
 		item := ui.NewTextbox(15, i%3+1, vec.ZERO_COORD, 1, "List item "+fmt.Sprint(i)+"/n", false)
-		ts.list.AddChild(&item)
+		ts.list.AddChild(item)
 	}
 
-	ts.list.EnableBorder("LIST", "")
+	ts.list.SetupBorder("LIST", "UP/DOWN/INSERT")
 	custom_border := ui.BorderStyles["Thin"]
 	custom_border.TextDecorationR = 'A'
 	custom_border.DisableLink = true
@@ -80,8 +76,29 @@ func (ts *TestState) Setup() {
 	ts.list.ToggleHighlight()
 	ts.list.ToggleScrollbar()
 	ts.list.SetDefaultColours(col.Pair{col.BLUE, col.WHITE})
+	page1.AddChild(ts.list)
 
-	ts.Window().AddChild(&ts.list)
+	choices := ui.NewChoiceBox(10, 1, vec.Coord{25, 10}, 5, "choice 1", "2", "really long choice")
+	choices.SetDefaultColours(col.Pair{col.GREY, col.MAROON})
+	page1.AddChild(choices)
+
+	//
+	// container test. multiple elements, including one that can move with the arrow keys
+	//
+	page2 := ts.tabs.CreatePage("container")
+	container := ui.ElementPrototype{}
+	container.Init(20, 15, vec.Coord{1, 1}, 1)
+	container.SetupBorder("Container", "")
+
+	ts.moving_text = ui.NewTextbox(ui.FIT_TEXT, ui.FIT_TEXT, vec.Coord{1, 1}, 0, "TEST STRING DO NOT UPVOTE", true)
+	ts.moving_text.SetDefaultColours(col.Pair{col.CYAN, col.FUSCHIA})
+	ts.moving_text.SetupBorder("TEST TITLE", "hint")
+	ts.moving_text.SetBorderStyle(ui.BORDER_STYLE_INHERIT)
+
+	text2 := ui.NewTextbox(10, ui.FIT_TEXT, vec.Coord{7, 3}, 0, util.LoremIpsum(31), true)
+	text2.SetupBorder("", "")
+	container.AddChildren(ts.moving_text, text2)
+	page2.AddChild(&container)
 }
 
 func (ts *TestState) Update() {
@@ -92,24 +109,33 @@ func (ts *TestState) UpdateUI() {
 	return
 }
 
+func (ts *TestState) GiveUserSeizure() {
+	flash := gfx.NewFlashAnimation(ts.Window().Bounds(), 0, col.Pair{col.WHITE, col.WHITE}, 10)
+	flash.OneShot = true
+	flash.Start()
+	ts.Window().AddAnimation(flash)
+}
+
 func (ts *TestState) HandleInputs(e event.Event) {
 	switch e.ID() {
 	case input.EV_KEYBOARD:
 		ev := e.(input.KeyboardEvent)
 		switch ev.Key {
-		case input.K_a:
-			// item := ui.NewTextbox(15, 1, vec.ZERO_COORD, 1, "new item", false)
-			// ts.list.AddChild(&item)
+		case input.K_INSERT:
+			item := ui.NewTextbox(15, 1, vec.ZERO_COORD, 1, "new item", false)
+			ts.list.AddChild(item)
 		case input.K_RIGHT:
-			ts.text.Move(1, 0)
+			ts.moving_text.Move(1, 0)
 		case input.K_LEFT:
-			ts.text.Move(-1, 0)
+			ts.moving_text.Move(-1, 0)
 		case input.K_UP:
-			ts.text.Move(0, -1)
+			ts.moving_text.Move(0, -1)
 		case input.K_DOWN:
-			ts.text.Move(0, 1)
+			ts.moving_text.Move(0, 1)
 		case input.K_RETURN:
-			ts.text.ToggleVisible()
+			ts.moving_text.ToggleVisible()
+		case input.K_F1:
+			ts.GiveUserSeizure()
 		}
 	}
 }
